db6dc932a3bf22d530242b79a44585a693c50eab
roselyne mwaniki <roselynewairimu30@gmail.com>
2024-11-28 22:38:00 +0300

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,32 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+        self._name = None
+        self._breed = None
+        self.name = name  # Triggers validation
+        self.breed = breed  # Triggers validation
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if value is not None:  # Allow None for uninitialized attribute
+            if not isinstance(value, str) or not (1 <= len(value) <= 25):
+                print("Name must be string between 1 and 25 characters.")
+            else:
+                self._name = value
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value is not None:  # Allow None for uninitialized attribute
+            if value not in APPROVED_BREEDS:
+                print("Breed must be in list of approved breeds.")
+            else:
+                self._breed = value
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,32 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name=None, job=None):
+        self._name = None
+        self._job = None
+        self.name = name  # Triggers validation
+        self.job = job  # Triggers validation
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if value is not None:  # Allow None for uninitialized attribute
+            if not isinstance(value, str) or not (1 <= len(value) <= 25):
+                print("Name must be string between 1 and 25 characters.")
+            else:
+                self._name = value.title()  # Convert valid name to title case
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value is not None:  # Allow None for uninitialized attribute
+            if value not in APPROVED_JOBS:
+                print("Job must be in list of approved jobs.")
+            else:
+                self._job = value
